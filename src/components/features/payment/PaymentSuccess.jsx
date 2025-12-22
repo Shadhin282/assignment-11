c1070fd8ba8cb058cb33ac98cc368404a73970fa
shadin khan <13307041@iubat.edu>
2025-12-22 21:57:59 +0600

    payment success update

--- src/components/features/payment/PaymentSuccess.jsx
+++ src/components/features/payment/PaymentSuccess.jsx
@@ -13,18 +13,18 @@ const PaymentSuccess = () => {
     }
   }, [sessionId])
   return (
-    <div className='flex flex-col items-center justify-center'>
-      <div className='bg-white p-10 rounded-lg shadow-lg text-center'>
+    <div className='flex flex-col items-center justify-center my-20'>
+      <div className='bg-linear-to-br from-indigo-900 via-slate-900 to-slate-950 p-10 rounded-lg shadow-xl text-center'>
         <IoBagCheckOutline className='w-16 h-16 text-green-500 mx-auto mb-4' />
-        <h1 className='text-3xl font-bold text-gray-800 mb-2'>
+        <h1 className='text-3xl font-bold text-white mb-2'>
           Payment Successful!
         </h1>
-        <p className='text-gray-600 mb-6'>
+        <p className='text-gray-400 mb-6'>
           Thank you for your purchase. Your order is being processed.
         </p>
         <Link
           to='/dashboard/user/participated'
-          className='inline-block bg-lime-500 text-white font-semibold py-2 px-4 rounded hover:bg-lime-600 transition duration-300'
+          className='bg-blue-600 text-white px-6 py-3 rounded-lg font-medium hover:bg-blue-700 transition-colors'
         >
           Go to My Orders
         </Link>
